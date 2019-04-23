71cbcfcc872ae66725cc4cb1169b69603195fc94
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-23 23:57:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,8 +11,8 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
-  speakers.each.with_index do |speaker, index|
-    return "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  speakers.each do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   end
 end
   
