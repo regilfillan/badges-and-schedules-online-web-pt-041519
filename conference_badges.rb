4e49e17cf065fd21c731cce32c79a2eb47fe68a0
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-23 23:53:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 speakers= ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
-def badge_maker("name")
-  puts "Hello, my name is #{name}."
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(speakers)
